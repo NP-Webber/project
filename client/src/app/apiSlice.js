7c0085ed442f952f474a0fb857694f106ae9679b
NP-Webber <7184551@gmail.com>
2024-03-13 01:25:01 +0200

    סידור מסמך

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -1,11 +1,11 @@
-import {createApi,fetchBaseQuery}from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const apiSlice=createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({
-        baseUrl:"http://localhost:1110"
+const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:1110"
     }),
-    endpoints:()=>({})
+    endpoints: () => ({})
 })
 
 export default apiSlice
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import{Provider} from "react-redux"
+import { Provider } from "react-redux"
 import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
